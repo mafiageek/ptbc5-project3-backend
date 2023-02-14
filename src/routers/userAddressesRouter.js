88f3f0821c1f74900e4411b318cd094313f40ce2
boxyboxy <eugenengboxiang@gmail.com>
2023-02-14 19:45:08 +0800

    fixed addressRouter spaces

--- src/routers/userAddressesRouter.js
+++ src/routers/userAddressesRouter.js
@@ -6,8 +6,6 @@ const userAddressesRouter = express.Router();
 
 userAddressesRouter.get("/", userAddressesController.getAllAddresses);
 userAddressesRouter.get("/:id", userAddressesController.getAddressById);
-
 userAddressesRouter.patch("/:id", userAddressesController.updateAddressById);
-
 userAddressesRouter.post("", userAddressesController.createUserAddress);
 module.exports = userAddressesRouter;
